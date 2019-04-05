c96e2024d27888e590949956ac7999099ce8517c
Dongare <akshay.dongare@capgemini.com>
2019-04-05 20:02:50 +0530

    models added

---
+++ src/app/pages/inventory/shared/models/item.ts
@@ -0,0 +1,10 @@
+export class Item {
+    id: number ;
+    name: string ;
+    code : string ;
+    quantity : number;
+    unit: string ;
+    price: number ;
+    desc : string ;
+    category: string 
+}
